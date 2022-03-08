b164b01b50dfcea0ebccaffbdaf5b0cf9b16929c
0000u <97921975+0000u@users.noreply.github.com>
2022-03-08 20:09:03 +0900

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,52 @@
+
+function log(msg){
+	console.log(msg);
+}
+
+
+log(1);
+//displayData(data, filter);
+getDataAsync('http://aengji.dothome.co.kr/fifa/index.html').then(data => {
+	log(data);
+});
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
+    }
+}
+
+
+
+
+async function getDataAsync(url) {
+	if(!url){
+		return;
+	}
+	var response = await fetch(url);
+	var data = await response.json();
+	return data;
+}
+
+function getData(url = '') {
+	return fetch(url)
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		return data;
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
