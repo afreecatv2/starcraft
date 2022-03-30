11f3d43c6bb5c1cc553fbfd55af517c72463dda5
afreecatv2 <101603251+afreecatv2@users.noreply.github.com>
2022-03-31 04:33:45 +0900

    Add files via upload

--- js/text.js
+++ js/text.js
@@ -1,6 +1,6 @@
 function initparticles() {
    // bubbles();
-   // hearts();
+   hearts();
    // lines();
    confetti();
    // fire();
@@ -23,7 +23,7 @@ function hearts() {
    $.each($(".particletext.hearts"), function(){
       var heartcount = ($(this).width()/50)*5;
       for(var i = 0; i <= heartcount; i++) {
-         var size = ($.rnd(60,120)/10);
+         var size = ($.rnd(60,120)/5);
          $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
       }
    });
