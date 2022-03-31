16480f551339f8f70886d48b19604c8f771b1448
afreecatv2 <101603251+afreecatv2@users.noreply.github.com>
2022-03-31 09:23:00 +0900

    Add files via upload

--- js/text.js
+++ js/text.js
@@ -10,66 +10,123 @@ function initparticles() {
 /*The measurements are ... whack (so to say), for more general text usage I would generate different sized particles for the size of text; consider this pen a POC*/
 
 function bubbles() {
-   $.each($(".particletext.bubbles"), function(){
-      var bubblecount = ($(this).width()/50)*10;
-      for(var i = 0; i <= bubblecount; i++) {
-         var size = ($.rnd(40,80)/10);
-         $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.bubbles"), function(){
+      // var bubblecount = ($(this).width()/50)*10;
+      // for(var i = 0; i <= bubblecount; i++) {
+         // var size = ($.rnd(40,80)/10);
+         // $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+      // }
+   // });
+   
+   var ts = document.querySelectorAll('.particletext.bubbles');
+	for(var t of ts){
+		var bubblecount = (t.offsetWidth / 50) * 10;
+		for (var i = 0; i <= bubblecount; i++) {
+			var size = (rnd(40,80)/10);
+			ce(t, 'span',{'class':'particle','style':'top:' + rnd(20, 80) + '%; left:' + rnd(0, 95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + (rnd(0, 30) / 10) + 's;'});
+		}
+	}
 }
 
 function hearts() {
-   $.each($(".particletext.hearts"), function(){
-      var heartcount = ($(this).width()/50)*5;
-      for(var i = 0; i <= heartcount; i++) {
-         var size = ($.rnd(60,120)/5);
-         $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.hearts"), function(){
+      // var heartcount = ($(this).width()/50)*5;
+      // for(var i = 0; i <= heartcount; i++) {
+         // var size = ($.rnd(60,120)/5);
+         // $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+      // }
+   // });
+   
+   var ts = document.querySelectorAll('.particletext.hearts');
+	for(var t of ts){
+		var heartcount = (t.offsetWidth / 50) * 5;
+		for (var i = 0; i <= heartcount; i++) {
+			// var size = (rnd(60, 120) / 10);
+			var size = (rnd(60, 120) / 5);
+			ce(t, 'span',{'class':'particle','style':'top:' + rnd(20, 80) + '%; left:' + rnd(0, 95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + (rnd(0, 30) / 10) + 's;'});
+		}
+	}
 }
 
 function lines() {
-   $.each($(".particletext.lines"), function(){
-      var linecount = ($(this).width()/50)*10;
-      for(var i = 0; i <= linecount; i++) {
-         $(this).append('<span class="particle" style="top:' + $.rnd(-30,30) + '%; left:' + $.rnd(-10,110) + '%;width:' + $.rnd(1,3) + 'px; height:' + $.rnd(20,80) + '%;animation-delay: -' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.lines"), function(){
+      // var linecount = ($(this).width()/50)*10;
+      // for(var i = 0; i <= linecount; i++) {
+         // $(this).append('<span class="particle" style="top:' + $.rnd(-30,30) + '%; left:' + $.rnd(-10,110) + '%;width:' + $.rnd(1,3) + 'px; height:' + $.rnd(20,80) + '%;animation-delay: -' + ($.rnd(0,30)/10) + 's;"></span>');
+      // }
+   // });
+   var ts = document.querySelectorAll('.particletext.lines');
+	for(var t of ts){
+		var linecount = (t.offsetWidth / 50) * 10;
+		for (var i = 0; i <= linecount; i++) {
+			var size = (rnd(40,80)/10);
+			ce(t, 'span',{'class':'particle','style':'top:' + rnd(-30,30) + '%; left:' + rnd(-10,110) + '%;width:' + rnd(1,3) + 'px; height:' + rnd(20,80) + '%;animation-delay: -' + (rnd(0,30)/10) + 's;'});
+		}
+	}
 }
 
 function confetti() {
-   $.each($(".particletext.confetti"), function(){
-      var confetticount = ($(this).width()/50)*10;
-      for(var i = 0; i <= confetticount; i++) {
-         $(this).append('<span class="particle c' + $.rnd(1,2) + '" style="top:' + $.rnd(10,50) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(12,16) + 'px; height:' + $.rnd(6,8) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.confetti"), function(){
+      // var confetticount = ($(this).width()/50)*10;
+      // for(var i = 0; i <= confetticount; i++) {
+         // $(this).append('<span class="particle c' + $.rnd(1,2) + '" style="top:' + $.rnd(10,50) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(12,16) + 'px; height:' + $.rnd(6,8) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+      // }
+   // });
+   var ts = document.querySelectorAll('.particletext.confetti');
+	for(var t of ts){
+		var confetticount = (t.offsetWidth / 50) * 10;
+		for (var i = 0; i <= confetticount; i++) {
+			var size = (rnd(40,80)/10);
+			ce(t, 'span',{'class':'particle c' + rnd(1,2),'style':'top:' + rnd(10,50) + '%; left:' + rnd(0,100) + '%;width:' + rnd(12,16) + 'px; height:' + rnd(6,8) + 'px;animation-delay: ' + (rnd(0,30)/10) + 's;'});
+		}
+	}
 }
 
 function fire() {
-   $.each($(".particletext.fire"), function(){
-      var firecount = ($(this).width()/50)*20;
-      for(var i = 0; i <= firecount; i++) {
-         var size = $.rnd(8,12);
-         $(this).append('<span class="particle" style="top:' + $.rnd(40,70) + '%; left:' + $.rnd(-10,100) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,20)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.fire"), function(){
+      // var firecount = ($(this).width()/50)*20;
+      // for(var i = 0; i <= firecount; i++) {
+         // var size = $.rnd(8,12);
+         // $(this).append('<span class="particle" style="top:' + $.rnd(40,70) + '%; left:' + $.rnd(-10,100) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,20)/10) + 's;"></span>');
+      // }
+   // });
+   var ts = document.querySelectorAll('.particletext.fire');
+	for(var t of ts){
+		var firecount = (t.offsetWidth / 50) * 10;
+		for (var i = 0; i <= firecount; i++) {
+			var size = (rnd(40,80)/10);
+			ce(t, 'span',{'class':'particle','style':'top:' + rnd(40,70) + '%; left:' + rnd(-10,100) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + (rnd(0,20)/10) + 's;'});
+		}
+	}
 }
 
 function sunbeams() {
-   $.each($(".particletext.sunbeams"), function(){
-      var linecount = ($(this).width()/50)*10;
-      for(var i = 0; i <= linecount; i++) {
-         $(this).append('<span class="particle" style="top:' + $.rnd(-50,0) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(1,3) + 'px; height:' + $.rnd(80,160) + '%;animation-delay: -' + ($.rnd(0,30)/10) + 's;"></span>');
-      }
-   });
+   // $.each($(".particletext.sunbeams"), function(){
+      // var linecount = ($(this).width()/50)*10;
+      // for(var i = 0; i <= linecount; i++) {
+         // $(this).append('<span class="particle" style="top:' + $.rnd(-50,0) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(1,3) + 'px; height:' + $.rnd(80,160) + '%;animation-delay: -' + ($.rnd(0,30)/10) + 's;"></span>');
+      // }
+   // });
+   var ts = document.querySelectorAll('.particletext.sunbeams');
+	for(var t of ts){
+		var linecount = (t.offsetWidth / 50) * 10;
+		for (var i = 0; i <= linecount; i++) {
+			var size = (rnd(40,80)/10);
+			ce(t, 'span',{'class':'particle','style':'top:' + rnd(-50,0) + '%; left:' + rnd(0,100) + '%;width:' + $.rnd(1,3) + 'px; height:' + rnd(80,160) + '%;animation-delay: -' + (rnd(0,30)/10) + 's;'});
+		}
+	}
 }
 
-jQuery.rnd = function(m,n) {
-      m = parseInt(m);
+// jQuery.rnd = function(m,n) {
+      // m = parseInt(m);
+      // n = parseInt(n);
+      // return Math.floor( Math.random() * (n - m + 1) ) + m;
+// }
+function rnd(m, n){
+	m = parseInt(m);
       n = parseInt(n);
       return Math.floor( Math.random() * (n - m + 1) ) + m;
 }
 
 initparticles();
+
