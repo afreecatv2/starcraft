ea166a7b70dda578c6a37d86f99aea593d74ff25
afreecatv2 <101603251+afreecatv2@users.noreply.github.com>
2022-07-05 13:16:07 +0900

    Add files via upload

---
+++ js/modify_notice.js
@@ -0,0 +1,35 @@
+function postData(url = '', name, notice) {
+	return fetch(url, {
+    method: 'POST',
+	headers: {'Content-Type':'application/x-www-form-urlencoded'},
+    body: 'name=' + name + '&notice=' + notice
+  })
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		return data;
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
+function modify_notice(){
+	const sp_notice = document.getElementById('sp_notice');
+	const ta_notice = document.getElementById('ta_notice');
+	if(event.target.textContent == '수정하기'){		
+		event.target.textContent = '저장';
+		ta_notice.value = sp_notice.textContent;
+		sp_notice.style.display = 'none';
+		document.getElementById('ta_notice').style.display = 'block';
+	} else {	
+		event.target.textContent = '수정하기';
+		sp_notice.textContent = ta_notice.value;
+		sp_notice.style.display = 'block';
+		ta_notice.style.display = 'none';
+		event.target.parentNode.parentNode.style.width = '200px';
+		
+		postData('https://test.aengji.com/afreecatv/afreecatv_notice_insert.php', univ_name, ta_notice.value);
+	}
+}
